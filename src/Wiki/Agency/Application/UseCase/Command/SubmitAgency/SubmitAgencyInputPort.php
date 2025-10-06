7ff02cd13f71e1beeaff6103314d92201e4cf9d1
takehiroito <takehiro.ito@smaregi.jp>
2025-10-06 19:55:30 +0900

    update: 事務所の変更申請に認可処理を追加

--- src/Wiki/Agency/Application/UseCase/Command/SubmitAgency/SubmitAgencyInputPort.php
+++ src/Wiki/Agency/Application/UseCase/Command/SubmitAgency/SubmitAgencyInputPort.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace Source\Wiki\Agency\Application\UseCase\Command\SubmitAgency;
 
 use Source\Wiki\Agency\Domain\ValueObject\AgencyIdentifier;
+use Source\Wiki\Shared\Domain\Entity\Principal;
 
 interface SubmitAgencyInputPort
 {
     public function agencyIdentifier(): AgencyIdentifier;
+
+    public function principal(): Principal;
 }
