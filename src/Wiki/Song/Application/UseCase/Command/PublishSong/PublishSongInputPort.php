5a6f9508a15c8146150f21244e7fad3bb278b686
takehiroito <takehiro.ito@smaregi.jp>
2025-10-07 23:58:03 +0900

    update: 歌の公開に認可処理を追加

--- src/Wiki/Song/Application/UseCase/Command/PublishSong/PublishSongInputPort.php
+++ src/Wiki/Song/Application/UseCase/Command/PublishSong/PublishSongInputPort.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Source\Wiki\Song\Application\UseCase\Command\PublishSong;
 
+use Source\Wiki\Shared\Domain\Entity\Principal;
 use Source\Wiki\Song\Domain\ValueObject\SongIdentifier;
 
 interface PublishSongInputPort
@@ -11,4 +12,6 @@ interface PublishSongInputPort
     public function songIdentifier(): SongIdentifier;
 
     public function publishedSongIdentifier(): ?SongIdentifier;
+
+    public function principal(): Principal;
 }
