1de85feb255e05c8b005e90601a3ba7945ffe5d7
takehiroito <takehiro.ito@smaregi.jp>
2025-10-07 23:29:09 +0900

    update: 歌の編集に認可処理を追加

--- src/Wiki/Song/Application/UseCase/Command/EditSong/EditSongInterface.php
+++ src/Wiki/Song/Application/UseCase/Command/EditSong/EditSongInterface.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Source\Wiki\Song\Application\UseCase\Command\EditSong;
 
+use Source\Wiki\Shared\Domain\Exception\UnauthorizedException;
 use Source\Wiki\Song\Application\Exception\SongNotFoundException;
 use Source\Wiki\Song\Domain\Entity\DraftSong;
 
@@ -13,6 +14,7 @@ interface EditSongInterface
      * @param EditSongInputPort $input
      * @return DraftSong
      * @throws SongNotFoundException
+     * @throws UnauthorizedException
      */
     public function process(EditSongInputPort $input): DraftSong;
 }
