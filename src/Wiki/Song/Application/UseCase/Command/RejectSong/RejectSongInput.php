38aa1aaecbcd985afdfe7ca65e46e4f4f5bd52ea
takehiroito <takehiro.ito@smaregi.jp>
2025-10-07 23:50:16 +0900

    update: 歌の変更拒否に認可処理を追加

--- src/Wiki/Song/Application/UseCase/Command/RejectSong/RejectSongInput.php
+++ src/Wiki/Song/Application/UseCase/Command/RejectSong/RejectSongInput.php
@@ -4,17 +4,24 @@ declare(strict_types=1);
 
 namespace Source\Wiki\Song\Application\UseCase\Command\RejectSong;
 
+use Source\Wiki\Shared\Domain\Entity\Principal;
 use Source\Wiki\Song\Domain\ValueObject\SongIdentifier;
 
 readonly class RejectSongInput implements RejectSongInputPort
 {
     public function __construct(
         private SongIdentifier $songIdentifier,
+        private Principal      $principal,
     ) {
     }
 
     public function songIdentifier(): SongIdentifier
     {
         return $this->songIdentifier;
+    }
+
+    public function principal(): Principal
+    {
+        return $this->principal;
     }
 }
