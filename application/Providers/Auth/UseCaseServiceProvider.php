2a08c6fe26bfd41f8fbb82478ede5293bc1b08a8
takehiroito <takehiro.ito@smaregi.jp>
2025-11-16 15:53:01 +0900

    create: ログアウトユースケースの作成

--- application/Providers/Auth/UseCaseServiceProvider.php
+++ application/Providers/Auth/UseCaseServiceProvider.php
@@ -7,11 +7,14 @@ namespace Application\Providers\Auth;
 use Illuminate\Support\ServiceProvider;
 use Source\Auth\Application\UseCase\Command\Login\Login;
 use Source\Auth\Application\UseCase\Command\Login\LoginInterface;
+use Source\Auth\Application\UseCase\Command\Logout\Logout;
+use Source\Auth\Application\UseCase\Command\Logout\LogoutInterface;
 
 class UseCaseServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
         $this->app->singleton(LoginInterface::class, Login::class);
+        $this->app->singleton(LogoutInterface::class, Logout::class);
     }
 }
