82298f7c82dcc0d9dc1a8115148e01bad63b961f
SHINJO\ryuse <shinjo0015@gmail.com>
2025-11-24 02:12:52 +0900

    no message

--- database/migrations/2024_01_01_000002_create_groups_table.php
+++ database/migrations/2024_01_01_000002_create_groups_table.php
@@ -13,6 +13,19 @@ return new class extends Migration
     {
         Schema::create('groups', static function (Blueprint $table) {
             $table->string('id', 26)->primary()->comment('グループID');
+            $table->string('translation_set_identifier', 26)->comment('翻訳セットID');
+            $table->string('translation', 8)->comment('翻訳言語');
+            $table->string('name', 32)->comment('グループ名');
+            $table->string('agency_id', 26)->nullable()->comment('所属事務所ID');
+            $table->text('description')->comment('概要')->default('');
+            $table->json('song_identifiers')->comment('楽曲ID一覧')->default('[]');
+            $table->string('image_path', 255)->nullable()->comment('画像パス');
+            $table->unsignedInteger('version')->comment('バージョン');
+            $table->timestamps();
+        });
+
+        Schema::create('draft_groups', static function (Blueprint $table) {
+            $table->string('id', 26)->primary()->comment('グループID');
             $table->string('published_id', 26)->nullable()->comment('公開済みグループID');
             $table->string('translation_set_identifier', 26)->comment('翻訳セットID');
             $table->string('editor_id', 26)->nullable()->comment('編集者ID');
@@ -34,6 +47,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('groups');
+        Schema::dropIfExists('draft_groups');
     }
 };
 
