d35fa1c07ee0687cb3cc4d41d53ad34341e53906
takehiroito <takehiro.ito@smaregi.jp>
2026-01-01 18:01:50 +0900

    fix: importミスを修正

--- application/Models/Wiki/DraftSong.php
+++ application/Models/Wiki/DraftSong.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace Application\Models\Wiki;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Ramsey\Collection\Collection;
 
 /**
  * @property string $id
