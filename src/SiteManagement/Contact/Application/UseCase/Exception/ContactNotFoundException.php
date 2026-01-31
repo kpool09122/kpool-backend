d5dd3b8e0e3eb6701499cbb74630af015bd2e3b2
SHINJO\ryuse <shinjo0015@gmail.com>
2026-01-31 11:19:53 +0900

    update: 例外を終えるようにpreviosを追加

--- src/SiteManagement/Contact/Application/UseCase/Exception/ContactNotFoundException.php
+++ src/SiteManagement/Contact/Application/UseCase/Exception/ContactNotFoundException.php
@@ -5,11 +5,15 @@ declare(strict_types=1);
 namespace Source\SiteManagement\Contact\Application\UseCase\Exception;
 
 use Exception;
+use Throwable;
 
 class ContactNotFoundException extends Exception
 {
-    public function __construct(string $message = 'Contact not found')
-    {
-        parent::__construct($message);
+    public function __construct(
+        string $message = 'Contact not found',
+        int $code = 0,
+        ?Throwable $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
     }
 }
