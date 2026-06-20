bf6142dd1c028cb62275a0afcc9e123d8554110b
shinjo15 <shinjo0015@gmail.com>
2026-06-20 15:11:38 +0900

    fix: address reply contact review comments

--- application/Providers/SiteManagement/DomainServiceProvider.php
+++ application/Providers/SiteManagement/DomainServiceProvider.php
@@ -6,6 +6,7 @@ namespace Application\Providers\SiteManagement;
 
 use Illuminate\Support\ServiceProvider;
 use Source\Shared\Application\Service\Encryption\EncryptionServiceInterface;
+use Source\Shared\Domain\ValueObject\Email;
 use Source\Shared\Infrastructure\Service\Encryption\EncryptionService;
 use Source\SiteManagement\Announcement\Domain\Factory\AnnouncementFactoryInterface;
 use Source\SiteManagement\Announcement\Domain\Factory\DraftAnnouncementFactoryInterface;
@@ -17,10 +18,12 @@ use Source\SiteManagement\Contact\Domain\Factory\ContactFactoryInterface;
 use Source\SiteManagement\Contact\Domain\Factory\ReplyContactFactoryInterface;
 use Source\SiteManagement\Contact\Domain\Repository\ContactRepositoryInterface;
 use Source\SiteManagement\Contact\Domain\Repository\ReplyContactRepositoryInterface;
+use Source\SiteManagement\Contact\Domain\Service\EmailServiceInterface;
 use Source\SiteManagement\Contact\Infrastructure\Adapters\Repository\ContactRepository;
 use Source\SiteManagement\Contact\Infrastructure\Adapters\Repository\ReplyContactRepository;
 use Source\SiteManagement\Contact\Infrastructure\Factory\ContactFactory;
 use Source\SiteManagement\Contact\Infrastructure\Factory\ReplyContactFactory;
+use Source\SiteManagement\Contact\Infrastructure\Service\ContactEmailService;
 use Source\SiteManagement\User\Domain\Factory\UserFactoryInterface;
 use Source\SiteManagement\User\Domain\Repository\UserRepositoryInterface;
 use Source\SiteManagement\User\Infrastructure\Factory\UserFactory;
@@ -40,5 +43,11 @@ class DomainServiceProvider extends ServiceProvider
         $this->app->singleton(ContactRepositoryInterface::class, ContactRepository::class);
         $this->app->singleton(ReplyContactRepositoryInterface::class, ReplyContactRepository::class);
         $this->app->singleton(EncryptionServiceInterface::class, EncryptionService::class);
+        $this->app->singleton(
+            EmailServiceInterface::class,
+            fn (): ContactEmailService => new ContactEmailService(
+                new Email((string) config('mail.from.address'))
+            )
+        );
     }
 }
--- src/SiteManagement/Contact/Application/UseCase/Command/ReplyContact/ReplyContactInterface.php
+++ src/SiteManagement/Contact/Application/UseCase/Command/ReplyContact/ReplyContactInterface.php
@@ -6,12 +6,14 @@ namespace Source\SiteManagement\Contact\Application\UseCase\Command\ReplyContact
 
 use Source\SiteManagement\Contact\Application\UseCase\Exception\ContactNotFoundException;
 use Source\SiteManagement\Contact\Application\UseCase\Exception\FailedToSendEmailException;
+use Source\SiteManagement\Shared\Domain\Exception\UnauthorizedException;
 
 interface ReplyContactInterface
 {
     /**
      * @throws ContactNotFoundException
      * @throws FailedToSendEmailException
+     * @throws UnauthorizedException
      */
     public function process(ReplyContactInputPort $input): void;
 }
