e0511224efbb5bb3f78b9e4117f6263fbe8251f5
shinjo15 <shinjo0015@gmail.com>
2026-09-01 14:31:35 +0900

    fix: 管理者用問い合わせ一覧のレビューに対応

--- application/Http/Action/SiteManagement/Contact/Query/ListContacts/ListContactsAction.php
+++ application/Http/Action/SiteManagement/Contact/Query/ListContacts/ListContactsAction.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Application\Http\Action\SiteManagement\Contact\Query\ListContactsByIdentity;
+namespace Application\Http\Action\SiteManagement\Contact\Query\ListContacts;
 
 use Application\Http\Context\ActorContext;
 use Application\Http\Exceptions\ForbiddenHttpException;
@@ -10,29 +10,29 @@ use Application\Http\Exceptions\InternalServerErrorHttpException;
 use Illuminate\Http\JsonResponse;
 use Psr\Log\LoggerInterface;
 use Source\Shared\Domain\ValueObject\IdentityIdentifier;
-use Source\SiteManagement\Contact\Application\UseCase\Query\ListContactsByIdentity\ListContactsByIdentityInput;
-use Source\SiteManagement\Contact\Application\UseCase\Query\ListContactsByIdentity\ListContactsByIdentityInterface;
-use Source\SiteManagement\Contact\Application\UseCase\Query\ListContactsByIdentity\ListContactsByIdentityOutput;
+use Source\SiteManagement\Contact\Application\UseCase\Query\ListContacts\ListContactsInput;
+use Source\SiteManagement\Contact\Application\UseCase\Query\ListContacts\ListContactsInterface;
+use Source\SiteManagement\Contact\Application\UseCase\Query\ListContacts\ListContactsOutput;
 use Source\SiteManagement\Shared\Domain\Exception\UnauthorizedException;
 use Symfony\Component\HttpFoundation\Response;
 use Throwable;
 
-readonly class ListContactsByIdentityAction
+readonly class ListContactsAction
 {
     public function __construct(
-        private ListContactsByIdentityInterface $listContactsByIdentity,
+        private ListContactsInterface $listContacts,
         private ActorContext $actorContext,
         private LoggerInterface $logger,
     ) {
     }
 
-    public function __invoke(ListContactsByIdentityRequest $request): JsonResponse
+    public function __invoke(ListContactsRequest $request): JsonResponse
     {
         try {
-            $output = new ListContactsByIdentityOutput();
-            $this->listContactsByIdentity->process(new ListContactsByIdentityInput(
+            $output = new ListContactsOutput();
+            $this->listContacts->process(new ListContactsInput(
                 $this->actorContext->identityIdentifier,
-                new IdentityIdentifier($request->identityIdentifier()),
+                $request->identityIdentifier() === null ? null : new IdentityIdentifier($request->identityIdentifier()),
             ), $output);
         } catch (UnauthorizedException $e) {
             $this->logger->error((string) $e);
